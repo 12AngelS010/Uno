ec3fbe12b61a769f833b06a767c29bdfef97f32a
Graham Thomas <grum@ladyp.com>
2018-11-23 16:46:43 +0000

    Consequence skip a go

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var port = (process.env.PORT || 6001);
+var port = (process.env.PORT || 6002);
 
 //var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
